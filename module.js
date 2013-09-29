6af4e1e7830458b908d730f61e89b24e08367a7f
Ryan Zec <code@ryanzec.com>
2013-09-28 21:03:07 -0400

    Inline documentation updates Dependency updates

--- module.js
+++ module.js
@@ -1,4 +1,8 @@
-//todo: document
+/**
+ * Nucleus Angular Single Panel
+ *
+ * @module nag.singlePanel
+ */
 angular.module('nag.singlePanel', [
     'nag.singlePanel.panel',
     'nag.singlePanel.manager'
